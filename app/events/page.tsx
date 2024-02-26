fad436f43fe2de1528d1b660e4cb997fed01326c
Kieran620 <kierank.pc@gmail.com>
2024-02-26 01:19:04 -0500

    APOSTROPHE FIX AGANE

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -29,7 +29,7 @@ const Events = () => {
                 <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white"></div>
                 <time className="mb-1 text-sm font-normal leading-none text-red-500 ">February 27th, 2024</time>
                 <h3 className="text-lg font-semibold text-gray-900">Soldering Workshop Hosted By Terrapin Works</h3>
-                <p className="text-base font-normal text-gray-500">Come learn basic and through-hole soldering! The club's President Amit Panchal will be helping host.</p>
+                <p className="text-base font-normal text-gray-500">Come learn basic and through-hole soldering! The President of DBI, Amit Panchal, will be helping host.</p>
             </li>
             <li className="ms-4">
                 <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white"></div>
